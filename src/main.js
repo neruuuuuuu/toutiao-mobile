2f148b06e0ed42d735ccf7ec01484bddb6079f92
neruuuuuuu <906413245@qq.com>
2021-05-12 16:12:18 +0800

    调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// wepack打包启动入口
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
