19fe1d64b5602abc1ecb962427ab3893c0a29dad
neruuuuuuu <906413245@qq.com>
2021-07-10 15:39:22 +0800

    利用axios拦截器统一设置Token

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 // 非组件模块获取 store
-import store from '../store/'
+// import store from '../store/'
 
 export const login = data => {
   return request({
@@ -22,12 +22,13 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    // 发送请求头数据
-    headers: {
-      // 注意：该接口需要授权才能访问
-      //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    url: '/app/v1_0/user'
+    // 未使用拦截器
+    // // 发送请求头数据
+    // headers: {
+    //   // 注意：该接口需要授权才能访问
+    //   //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,23 @@
 import axios from 'axios'
+// 非组件模块获取 store
+import store from '../store/'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+// Add a request interceptor
+request.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  const { user } = store.state
+  // 如果用户已登录，统一给接口设置 token 信息
+  if (user) {
+    config.headers.Authorization = 'Bearer {user.token}'
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
 })
 
 export default request
