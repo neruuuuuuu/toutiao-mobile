92de57c2ef3540e2aa15c62347bc0491f79dbd49
neruuuuuuu <906413245@qq.com>
2021-07-19 23:13:39 +0800

    封装获取搜索结果接口

--- src/api/search.js
+++ src/api/search.js
@@ -1,7 +1,9 @@
 
 import request from '@/utils/request'
 
-// 搜索联想建议
+/**
+ *搜索联想建议
+ */
 export const getSearchSuggestions = q => {
   return request({
     method: 'GET',
@@ -9,5 +11,16 @@ export const getSearchSuggestions = q => {
     params: {
       q
     }
+  })
+}
+
+/**
+ * 获取搜索结果
+ */
+export function getSearchResult (params) {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/search',
+    params
   })
 }
