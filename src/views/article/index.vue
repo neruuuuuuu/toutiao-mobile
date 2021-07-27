0aa408f21c5d23213ec3062760d7f9e7161d20a4
neruuuuuuu <906413245@qq.com>
2021-07-27 23:43:12 +0800

    文章详情页 关注，收藏，点赞

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -39,6 +39,8 @@
         round
         :type="article.is_followed ? 'default':'info'"
         :icon="article.is_followed ? '':'plus'"
+        @click="onFollow"
+        :loading="isLoading"
       >{{article.is_followed ? '已关注':'关注'}}</van-button>
     </van-cell>
     <!-- 文章正文 -->
@@ -50,14 +52,46 @@
     </div>
     <!-- 评论列表 -->
 
-    <!-- 操作 -->
+    <!-- 底部操作 -->
+    <div class="article-button">
+      <van-button
+        class="comment-btn"
+        type="default"
+        round
+        size="small"
+      >写评论</van-button>
+      <van-icon
+        name="comment-o"
+        color="#777"
+        size="22px"
+        info="99+"
+      ></van-icon>
+      <van-icon
+        :name="article.is_collected ? 'star' : 'star-o' "
+        :color="article.is_collected ? 'orange' : '#777' "
+        size="22px"
+        @click="onCollect"
+      ></van-icon>
+      <van-icon
+        :name="article.attitude === 1 ? 'good-job' : 'good-job-o' "
+        :color="article.attitude === 1 ? 'orange' : '#777' "
+        size="22px"
+        @click="onLike"
+      ></van-icon>
+      <van-icon
+        name="share-o"
+        color="#777"
+        size="22px"
+      ></van-icon>
+    </div>
   </div>
 </template>
 
 <script>
 import './github-markdown.css'
-import { getArticleById } from '@/api/article'
+import { getArticleById, addCollect, deleteCollect, addLike, deleteLike } from '@/api/article'
 import { ImagePreview } from 'vant'
+import { addFollow, deleteFollow } from '@/api/user.js'
 
 export default {
   name: 'ArticleIndex',
@@ -70,7 +104,8 @@ export default {
   },
   data () {
     return {
-      article: {}
+      article: {},
+      isLoading: false
     }
   },
   watch: {},
@@ -101,7 +136,68 @@ export default {
           })
         }
       })
+    },
+    // 关注事件方法
+    async onFollow () {
+      this.isLoading = true
+      if (this.article.is_followed) {
+        await deleteFollow(this.article.aut_id)
+      } else {
+        await addFollow(this.article.aut_id)
+      }
+      this.article.is_followed = !this.article.is_followed
+      this.isLoading = false
+    },
+    // 收藏事件方法
+    async onCollect () {
+      // 这里 loading 不仅仅是为了交互提示，更重要的是请求期间禁用背景点击功能，防止用户不断的操作界面发出请求
+      this.$toast.loading({
+        duration: 0, // 持续展示 toast
+        message: '收藏中...',
+        forbidClick: true // 是否禁止背景点击
+      })
+
+      try {
+        if (this.article.is_collected) {
+          await deleteCollect(this.articleId)
+          this.$toast.success('已取消收藏')
+        } else {
+          await addCollect(this.articleId)
+          this.$toast.success('收藏成功')
+        }
+        this.article.is_collected = !this.article.is_collected
+      } catch (err) {
+        console.log(err)
+        this.$toast.fail('操作失败')
+      }
+    },
+    // 文章点赞方法
+    async onLike () {
+      // 这里 loading 不仅仅是为了交互提示，更重要的是请求期间禁用背景点击功能，防止用户不断的操作界面发出请求
+      this.$toast.loading({
+        duration: 0, // 持续展示 toast
+        message: '点赞中...',
+        forbidClick: true // 是否禁止背景点击
+      })
+
+      try {
+        // 如果已经点赞，则取消点赞
+        if (this.article.attitude === 1) {
+          await deleteLike(this.articleId)
+          this.article.attitude = -1
+          this.$toast.success('取消点赞')
+        } else {
+          // 否则添加点赞
+          await addLike(this.articleId)
+          this.article.attitude = 1
+          this.$toast.success('点赞成功')
+        }
+      } catch (err) {
+        console.log(err)
+        this.$toast.fail('操作失败')
+      }
     }
+
   },
   created () {
     this.loadArticle()
@@ -133,6 +229,21 @@ export default {
   background-color: #fff;
   /deep/ p {
     text-align: justify;
+  }
+}
+.article-button {
+  background: #fff;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  .comment-btn {
+    width: 150px;
   }
 }
 </style>
