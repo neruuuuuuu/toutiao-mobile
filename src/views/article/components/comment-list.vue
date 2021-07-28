c0a1801a939b04cbefde952cfcf97bf96eeac8cb
neruuuuuuu <906413245@qq.com>
2021-07-28 21:12:53 +0800

    增加文章评论列表

---
+++ src/views/article/components/comment-list.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="comment-list">
+    <van-list
+      v-model="articleComment.loading"
+      :finished="articleComment.finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="(comment,index) in  articleComment.list"
+        :key="index"
+        :title="comment"
+      />
+    </van-list>
+  </div>
+</template>
+
+<script>
+import { getComments } from '@/api/comment'
+export default {
+  name: 'CommentList',
+  components: {},
+  props: {
+    articleId: {
+      type: [Number, String, Object],
+      require: true
+    }
+  },
+  data () {
+    return {
+      articleComment: { // 文章评论相关数据
+        list: [],
+        loading: false,
+        finished: false,
+        offset: null, // 请求下一页数据的页码
+        totalCount: 0 // 总数据条数
+      }
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    async onLoad () {
+      const articleComment = this.articleComment
+
+      const { data } = await getComments({
+        type: 'a', // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
+        source: this.articleId, // 源id，文章id或评论id
+        offset: articleComment.offset, // 获取评论数据的偏移量，值为评论id，表示从此id的数据向后取，不传表示从第一页开始读取数据
+        limit: 10 // 每页大小
+      })
+      console.log('评论')
+      console.log(data)
+
+      // 2. 将数据添加到列表中
+      const { results } = data.data
+      articleComment.list.push(...results)
+
+      // 更新总数据条数
+      articleComment.totalCount = data.data.total_count
+
+      // 3. 将加载更多的 loading 设置为 false
+      articleComment.loading = false
+
+      // 4. 判断是否还有数据
+      if (results.length) {
+        articleComment.offset = data.data.last_id // 更新获取下一页数据的页码
+      } else {
+        articleComment.finished = true // 没有数据了，关闭加载更多
+      }
+    }
+  },
+  created () { },
+  mounted () { }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -7,50 +7,58 @@
       @click-left="$router.back()"
       class="article-nav-bar"
     />
-    <!-- 文章标题 -->
-    <h1 class="article-title">
-      {{article.title}}
-    </h1>
-    <!-- 作者单元格 -->
-    <van-cell
-      class="auther-cell"
-      center
-    >
-      <!-- 作者头像 -->
-      <van-image
-        class="avatar"
-        slot="icon"
-        width="50"
-        height="50"
-        round
-        fit="cover"
-        :src="article.aut_photo"
-      />
-      <!-- 作者名 -->
+    <div class="article-wrap">
+      <!-- 文章标题 -->
+      <h1 class="article-title">
+        {{article.title}}
+      </h1>
+      <!-- 作者单元格 -->
+      <van-cell
+        class="auther-cell"
+        center
+      >
+        <!-- 作者头像 -->
+        <van-image
+          class="avatar"
+          slot="icon"
+          width="50"
+          height="50"
+          round
+          fit="cover"
+          :src="article.aut_photo"
+        />
+
+        <!-- 作者名 -->
+        <div
+          class="auther-name"
+          slot="title"
+        >{{article.aut_name}}</div>
+        <!-- 发布时间 -->
+        <div slot="label">{{article.pubdate | relativeTime}} </div>
+
+        <!-- 关注按钮 -->
+        <van-button
+          size="small"
+          round
+          :type="article.is_followed ? 'default':'info'"
+          :icon="article.is_followed ? '':'plus'"
+          @click="onFollow"
+          :loading="isLoading"
+        >{{article.is_followed ? '已关注':'关注'}}</van-button>
+      </van-cell>
+
+      <!-- 文章正文 -->
       <div
-        class="auther-name"
-        slot="title"
-      >{{article.aut_name}}</div>
-      <!-- 发布时间 -->
-      <div slot="label">{{article.pubdate | relativeTime}} </div>
-      <!-- 关注按钮 -->
-      <van-button
-        size="small"
-        round
-        :type="article.is_followed ? 'default':'info'"
-        :icon="article.is_followed ? '':'plus'"
-        @click="onFollow"
-        :loading="isLoading"
-      >{{article.is_followed ? '已关注':'关注'}}</van-button>
-    </van-cell>
-    <!-- 文章正文 -->
-    <div
-      ref="article-content"
-      class="markdown-bod"
-      v-html="article.content"
-    >
+        ref="article-content"
+        class="markdown-bod"
+        v-html="article.content"
+      >
+      </div>
+
+      <!-- 评论列表 -->
+      <comment-list :articleId="articleId"></comment-list>
+
     </div>
-    <!-- 评论列表 -->
 
     <!-- 底部操作 -->
     <div class="article-button">
@@ -92,10 +100,11 @@ import './github-markdown.css'
 import { getArticleById, addCollect, deleteCollect, addLike, deleteLike } from '@/api/article'
 import { ImagePreview } from 'vant'
 import { addFollow, deleteFollow } from '@/api/user.js'
+import CommentList from './components/comment-list.vue'
 
 export default {
   name: 'ArticleIndex',
-  components: {},
+  components: { CommentList },
   props: {
     articleId: {
       type: [Number, String, Object],
@@ -209,6 +218,14 @@ export default {
 <style lang="less" scoped>
 .article-nav-bar {
   background: rgb(255, 187, 199);
+}
+.article-wrap {
+  position: fixed;
+  top: 44px;
+  bottom: 48px;
+  left: 0;
+  right: 0;
+  overflow-y: auto;
 }
 .article-title {
   background: rgb(255, 255, 255);
